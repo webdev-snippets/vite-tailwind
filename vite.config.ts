9c37c5f2f2ca1e9dd361de8f8bc3b50c975ac3ee
Dialgatrainer02 <oliviabarker900@gmail.com>
2025-04-10 17:32:34 +0100

    add vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -12,5 +13,8 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src")
     },
     extensions: ['.tsx', '.mjs', '.cjs', '.js'],
+  },
+  test:{
+    globals: true
   }
 })
